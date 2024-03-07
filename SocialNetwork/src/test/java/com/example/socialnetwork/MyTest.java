92677a2c498e4a767efad1802806c867750c62b3
Nguyen Trung Nghia <ntn1804@gmail.com>
2024-03-07 22:37:22 +0700

    mimeType & resourceLoader

--- SocialNetwork/src/test/java/com/example/socialnetwork/MyTest.java
+++ SocialNetwork/src/test/java/com/example/socialnetwork/MyTest.java
@@ -2,18 +2,16 @@ package com.example.socialnetwork;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.support.StandardMultipartHttpServletRequest;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.io.InputStream;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@SpringBootTest
 public class MyTest {
 
     @Autowired
@@ -21,11 +19,14 @@ public class MyTest {
 
     @Test
     public void readResourceFile() throws IOException {
-//
 //        String name = "file";
 //        String disposition = "form-data; name=\"" + name + "\"; filename=\"myFile.txt\"";
 //        StandardMultipartHttpServletRequest request = requestWithPart(name, disposition, "myBody");
 //        MultipartFile multipartFile = request.getFile(name);
 //        assertNotNull(multipartFile);
+
+        Resource resource = resourceLoader.getResource("classpath:static/images/default-avatar.jpg");
+        var image = resource.getInputStream();
+        System.out.println(image);
     }
 }
